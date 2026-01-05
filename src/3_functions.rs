fb3c1103450d776a05ad5869e1abed6af0aa8254
noonyuu <noon3m.op.sh@gmail.com>
2026-01-06 00:56:06 +0900

    feat: 戻り値

--- src/3_functions.rs
+++ src/3_functions.rs
@@ -1,14 +1,36 @@
 fn main() {
-  println!("Hello, world!");
+  // println!("Hello, world!");
 
-  another_function();
-  another_function_with_parameters(5, 'x');
+  // another_function();
+  // another_function_with_parameters(5, 'x');
+
+
+  // let x = {
+  //   let y = 3;
+  //   // ここに式を書くことで値を返すことができる
+  //   // ;を書くと文になるからエラーになる
+  //   y + 1
+  // };
+
+  // println!("x is {x}");
+
+  // let x = five();
+  let x = plus_one(5);
+  println!("x is {x}");
 }
 
-fn another_function() {
-  println!("Another function.");
-}
+// fn another_function() {
+//   println!("Another function.");
+// }
 
-fn another_function_with_parameters(x: i32, y: char) {
-  println!("Another function with parameters: {x}, {y}");
+// fn another_function_with_parameters(x: i32, y: char) {
+//   println!("Another function with parameters: {x}, {y}");
+// }
+
+// fn five() -> i32 {
+//   5
+// }
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
