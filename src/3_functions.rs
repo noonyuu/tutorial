abf815adf27f4d49687a397f4fc6594f598611d0
noonyuu <noon3m.op.sh@gmail.com>
2026-01-06 00:52:39 +0900

    feat: 引数

--- src/3_functions.rs
+++ src/3_functions.rs
@@ -2,8 +2,13 @@ fn main() {
   println!("Hello, world!");
 
   another_function();
+  another_function_with_parameters(5, 'x');
 }
 
 fn another_function() {
   println!("Another function.");
+}
+
+fn another_function_with_parameters(x: i32, y: char) {
+  println!("Another function with parameters: {x}, {y}");
 }
